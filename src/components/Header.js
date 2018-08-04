44b290c35e70f585829e8b3cbb392ddc3c5cf365
yatyat2 <xowls2287@naver.com>
2018-08-04 17:00:34 +0900

    페이지이동 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "react-router-3";
 
 class Header extends React.Component {
   render() {
-    return <div>안녕 낭 헤더야</div>;
+    return (
+      <div>
+        <span className="Menu-Bar">
+          <Link to={"/"}>qna</Link>
+        </span>
+        <Link to={"meeting"}>미팅</Link>
+        <style JSX>
+          {`
+        .Menu-Bar {
+            margin-right:20px;
+        }
+        `}
+        </style>
+      </div>
+    );
   }
 }
 
