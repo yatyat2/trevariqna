789b8283a431e033a6bfebc6b1202ceddaea3214
yatyat2 <xowls2287@naver.com>
2018-08-04 14:34:33 +0900

    Quesiton 상태(선택or선택X)에 따른 CSS 추가

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 class Question extends React.Component {
+  state = {
+    isopen: false
+  };
+
+  renderanswer(isopen, answer) {
+    if (isopen) {
+      return <div>{answer}</div>;
+    }
+  }
+
+  openanswer() {
+    this.setState({
+      isopen: !this.state.isopen
+    });
+  }
+
   render() {
     const qnaData = this.props.qnaData;
-
+    const isopen = this.state.isopen;
     return (
-      <div className="question">
-        <span className="Q">Q.</span>
-        <span className="qnaCategory">{qnaData[0].category}</span>
-        <span>{qnaData[0].question}</span>
+      <div>
+        <div className="questionButton" onClick={() => this.openanswer()}>
+          <span className="Q">Q.</span>
+          <span className="qnaCategory">{qnaData[0].category}</span>
+          <span className={isopen ? "openQestion" : null}>
+            {qnaData[0].question}
+          </span>
+        </div>
+        <div className={isopen ? "answer" : null}>
+          {this.renderanswer(isopen, qnaData[0].answer)}
+        </div>
         <style JSX>
           {`
         .Q {
@@ -22,9 +45,17 @@ class Question extends React.Component {
             width:84px;
             float:left;
         }
-        .question {
+        .questionButton {
             border-bottom:1px dotted grey;
             padding:15px 10px 15px 10px;
+        }
+        .openQestion {
+            color:orange;
+            font-weight:bold;
+        }
+        .answer {
+            padding:20px 20px 40px 125px;
+            border-bottom:1px dotted grey;
         }
         `}
         </style>
