cf774ca5703b8e01528a40a0e9922bf57802ddb3
yatyat2 <xowls2287@naver.com>
2018-08-04 16:51:06 +0900

    페이지 이동을 위한 작업 1. react-router-3 패키지를설치 2. 컴포넌트와 컨테이너(컴포넌트를 가진 컴포넌트)를 분리하여 폴더생성 3. index.js 파일의 내용 App.js로 이동 4. index.js 파일에 URL에 따라 페이지이동을 하기 위한 react-router-3의 컴포넌트 추가

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return <div>안녕 낭 헤더야</div>;
+  }
+}
+
+export default Header;
---
+++ src/containers/Meeting.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+import React from "react";
+
+class Meeting extends React.Component {
+  render() {
+    return <div>미팅페이지이다.</div>;
+  }
+}
+
+export default Meeting;
